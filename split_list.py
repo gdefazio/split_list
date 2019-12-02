42e3dcc3f07e99662f090ead84d7f71bbf98ff0f
Giuseppe Defazio <48283042+gdefazio@users.noreply.github.com>
2019-12-02 14:18:19 +0100

    Create split_list.py

---
+++ split_list.py
@@ -0,0 +1,15 @@
+class list(list):
+
+    def split_list(self, chunks):
+        'Return a list containing the splitted input list in chunks'
+        length = len(self)
+        chunks = int(chunks)
+        chunklen = length / chunks
+        div = list(range(0, length, int(chunklen)))
+        div.append(None)
+        chunks_indexes = list(zip(div[:-1], div[1:]))
+        chunk_list = list()
+        for t in chunks_indexes:
+            chunk_list.append(self[t[0]:t[1]])
+
+        return chunk_list
